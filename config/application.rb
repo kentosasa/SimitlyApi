24b90d1919aa235ae2070fa27680444440e573a5
kento sasamoto <knote0721@gmail.com>
2015-10-31 12:44:35 +0900

    access-control-allow-origin

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module Api
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => '*'
+    }
   end
 end
